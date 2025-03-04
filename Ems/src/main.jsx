a2567fd1dbeed3fc77271dc4016e0364a38030d8
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-04 23:22:51 +0530

    update commit

--- Ems/src/main.jsx
+++ Ems/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TaskContext from './context/TaskContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+
+    <AuthContext>
+      <TaskContext>
+      <App />
+      </TaskContext>
+    </AuthContext>
+    
+  </React.StrictMode>,
 )
