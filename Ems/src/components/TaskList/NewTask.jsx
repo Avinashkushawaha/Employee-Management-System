b806bb55d2966a62e442e6a5eae0cc231615fc84
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-05 15:25:01 +0530

    update commit

--- Ems/src/components/TaskList/NewTask.jsx
+++ Ems/src/components/TaskList/NewTask.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 
 
-const NewTask = () => {
+const NewTask = ({data}) => {
   return (
     
-         <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+         <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
     <div className='flex justify-between items-center'>
-      <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-      <h4 className='text-sm'> 20 feb 2025</h4>
+      <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+      <h4 className='text-sm'>{data.taskDate}</h4>
     </div>
-    <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+    <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
      <p className='text-sm mt-2'>
-     Welcome to another video in our MERN Stack Full Course series! In this React project tutorial,
+     {data.taskDescription}
      </p>
-     <div className='mt-4'>
-        <button>Accept Task</button>
+     <div className='mt-6'>
+        <button className='bg-blue-500 rounded font-medium py-1 text-xs'>Accept Task</button>
      </div>
         </div>
   )
