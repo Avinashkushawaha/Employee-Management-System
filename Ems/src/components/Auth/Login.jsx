4e9753b8a51e38b049afba0bcb0afa6e10346ce5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-05 15:32:49 +0530

    update commit

--- Ems/src/components/Auth/Login.jsx
+++ Ems/src/components/Auth/Login.jsx
@@ -19,7 +19,7 @@ const Login = ({handleLogin}) => {
     <div className='flex h-screen w-screen items-center justify-center'>
     <div className='border-2 rounded-xl border-emerald-600 p-20'>
         <form 
-        onSubmit={submitHandler}
+        onSubmit={(e) =>{submitHandler(e)}}
         className='flex flex-col items-center justify-center'>
              <input
              value={email}
@@ -35,7 +35,7 @@ const Login = ({handleLogin}) => {
              }}
               required className='text-black outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 mt-2 rounded-full placeholder: text-black' type="password" placeholder='Enter your password'
                />
-             <button className= ' mt-5 text-white border-none outline-none   bg-emerald-600 text-xl py-3 px-5 rounded-full'>Log in</button>
+             <button className= ' mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
         </form> 
     </div>
 
--- Ems/src/components/Dashboard/EmployeeDashboard.jsx
+++ Ems/src/components/Dashboard/EmployeeDashboard.jsx
@@ -5,12 +5,12 @@ import TaskList from '../TaskList/TaskList'
 
 
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = (props) => {
   return (
-    <div className='w-full h-screen p-7'>
-        <Header />
-        <TaskListNumber />
-        <TaskList />
+    <div className='p-10 bg-[#1c1c1c] h-screen '>
+        <Header  changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumber  data={props.data}/>
+        <TaskList data={props.data} />
     </div>
   )
 }
